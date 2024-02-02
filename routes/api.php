d222e942d9c0721ce6440de50384d6cf20421769
Say-Full <saifullah.sani01@gmail.com>
2024-02-02 10:19:06 +0700

    Get semua data

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TesApi;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("tes", [TesApi::class, 'getData']);
+
+Route::get("list", [StudentController::class, 'list']);
 
